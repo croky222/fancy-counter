132c6a523694ad7b2c1b019d3d94246fac90cd51
croky222 <cnegrea05@gmail.com>
2025-03-29 13:06:42 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from "react";
+
+function App() {
+    return (
+        <React.Fragment>
+           <span>Hello Everyone</span>
+            <span>Hello Everyone x2</span>
+        </React.Fragment>
+    )
+}
+
+export default App
